dffe6fdad01c80cb229f1521e2a9d06d7ee5845d
Biswajit Mohapatra <biswajit.javaexpert@gmail.com>
2020-07-27 14:47:03 +0530

    correct formatting

--- src/main/java/com/biswajit/user/controller/ForgotPasswordController.java
+++ src/main/java/com/biswajit/user/controller/ForgotPasswordController.java
@@ -1,15 +1,26 @@
 package com.biswajit.user.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequestMapping("forgot-password")
 public class ForgotPasswordController {
 
-  @GetMapping
+ @GetMapping
   public String forgotPassword() {
+   return "emailForm";
+  }
+
+  @PostMapping
+  public String sendEmailLink(@RequestParam(value ="email",required = true) String email) {
+   log.info("Sending Email link to :{} ",email);
     return "emailForm";
   }
+
 }
